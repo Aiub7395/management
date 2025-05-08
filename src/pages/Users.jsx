1b138d1fe8c570370757a0099c4f41c3fc28a644
Aiub7395 <aiubali7395@gmail.com>
2025-05-08 12:58:29 +0300

    Update Users.jsx

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -239,7 +239,7 @@ export default function Owners() {
 										<button
 											onClick={() => setSelectedOwnerId(owner.id)}
 											className='bg-blue-500 text-white px-3 py-1 rounded-full hover:bg-blue-600 transition'>
-											Add Rent
+											Add Pay
 										</button>
 									</td>
 								</tr>
